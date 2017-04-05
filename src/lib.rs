4b5b63db3a0593fb030845a097ddb6c579eab1e3
Kevin Hoffman <alothien@gmail.com>
2017-04-05 08:01:39 -0400

    Adding documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,28 @@
 //! D20
 //!
+//! **D20** is a simple crate designed to evaluate _roll expressions_. A _roll expression_ is an
+//! english-language string that reflects the intent of a dungeon or game master to perform a 
+//! particular roll.
+//!
+//! For example, in a tabletop game you may frequently hear phrases like _"roll 2d10"_, or 
+//! _"roll 3d6 and add 5"_. These are roll expressions, and the components within them are
+//! what we call _die roll terms_. A _die roll term_ is either a term that calls for the rolling
+//! of an n-sided die x times (e.g. 3d6) or a modifier that simply adds or subtracts a constant value
+//! from the larger expression.
+//!
+//! Examples of valid _roll expressions_ include:
+//!
+//! * 3d6
+//! * 2d10 + 5
+//! * 1d20-3
+//! * +6
+//! * -2
+//! * 3d10+5d100-21+7
+//!
+//!
+//! Roll expressions can have arbitrary length and complexity, and it is perfectly legal for the final result
+//! of a roll expression to be negative after applying modifiers.
+//!
 //! # Examples
 //! ```
 //! extern crate d20;
@@ -7,6 +30,17 @@
 //! fn main() {
 //!     let r = d20::roll_dice("3d6 + 4").unwrap();
 //!     assert!(r.total > 6);
+//!     let r = d20::roll_dice("1d1-3").unwrap();
+//!     assert_eq!(r.total, -2);
+//!
+//!     let r = d20::roll_dice("roll four chickens and add six ferrets");
+//!     match r {
+//!        Ok(_) => assert!(false), // this should NOT be ok, fail
+//!        Err(_) => assert!(true), // bad expressions produce errors
+//!    }
+//!     
+//!     let rg = d20::roll_range(1,100).unwrap();
+//!     assert!(rg >= 1 && rg <= 100);
 //! }
 //! ```
 extern crate rand;
@@ -18,25 +52,39 @@ use regex::Regex;
 
 
 
-/// The `Roll` struct contains the `DieRollExpression`, the values of
-/// the individual rolls, and the calculated total. It is returned by the `roll_dice()`
+/// Represents the _results_ of an evaluated die roll expression. 
+/// 
+/// The `Roll` struct contains the original _die roll expression_ passed to the `roll_dice()`
 /// function.
 ///
-/// You can perform a roll mutliple times by converting it into an iterator.
+/// The list of `values` will always be a vector containing at least one element because roll 
+/// expressions are not valid without at least 1 term. Each resulting value is a tuple containing
+/// the parsed `DieRollTerm` and a vector of values. For `DieRollTerm::Modifier` terms, this will be a single-element
+/// vector containing the modifier value. For `DieRollTerm::DieRoll` terms, this will be a vector
+/// containing the results of each die roll.
+///
+/// The `total` field contains the net result of evaluating the entire roll expression.
+///
+/// You can evaluate a roll expression (perform a roll) mutliple times by converting it into an iterator.
 #[derive(Debug)]
 pub struct Roll {
-    // die roll expression
+    /// A die roll expression conforming to the format specification
     pub drex: String,
-    // individual die roll results
+    /// The results of evaluating each term in the expression
     pub values: Vec<(DieRollTerm, Vec<i8>)>,
-    // result of the drex
+    /// The net final result of evaluating all terms in the expression
     pub total: i32,
 }
 
 
+/// Formats roll results, including die rolls, in a human-readable string. 
+///
+/// For example, if the original expression was `3d6+5`, formatting the `Roll` struct
+/// might result in the following text:
+///
+/// `3d6[3,4,6]+5 (Total: 18)`
 impl fmt::Display for Roll {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // desired output: 3d6[6,4,3]+5 [Total: 18]
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {        
         let mut out = String::new();
 
         for i in 0..self.values.len() {
@@ -53,6 +101,8 @@ impl fmt::Display for Roll {
     }
 }
 
+/// Converts an evaluated roll expression into an iterator, allowing the expression
+/// to be evaluated (including re-rolling of dice) multiple times. 
 impl IntoIterator for Roll {
     type Item = Roll;
     type IntoIter = RollIterator;
@@ -65,7 +115,7 @@ impl IntoIterator for Roll {
     }
 }
 
-/// A `RollIterator` is created when `Roll.into_iter()` method is called.
+/// A `RollIterator` is created when `into_iter()` is called on a `Roll`.
 pub struct RollIterator {
     roll: Roll,
     index: usize,
@@ -86,17 +136,18 @@ impl Iterator for RollIterator {
     }
 }
 
-/// `DieRollTerm` represents an indifividual term within a die roll expression.
+/// Represents an individual term within a die roll expression. Terms can either be numeric
+/// modifiers like `+5` or `-2` or they can be terms indicating die rolls.
 #[derive(Debug, Clone)]
 pub enum DieRollTerm {
-    /// `DieRoll1` variant
+    /// Indicates a die roll term to roll `multiplier` dice with `sides` sides.
     DieRoll {
-        /// multiplier docs
+        /// Number of times to roll the given die
         multiplier: i8,
-        /// sides docs
+        /// Number of sides on the given die
         sides: u8,
     },
-    /// `Modifier` variant
+    /// Numeric modifier used in simple left-to-right numeric evaluation of a die roll expression.
     Modifier(i8),
 }
 
@@ -138,6 +189,9 @@ impl DieRollTerm {
     }
 }
 
+/// Formats an individual die roll term in a human-friendly fashion. For `Modifier` terms,
+/// this will force the printing of a + or - sign before the modifier value. For `DieRoll`
+/// terms, this displays the term in the form `5d10`. 
 impl fmt::Display for DieRollTerm {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
@@ -147,7 +201,9 @@ impl fmt::Display for DieRollTerm {
     }
 }
 
-/// `roll_dice()` will evaluate the string input as a die roll expression (e.g. 3d6 + 4).
+/// Evaluates the expression string input as a die roll expression (e.g. 3d6 + 4). The
+/// results are returned in a `Result` object that contains either a valid `Roll` or some
+/// text indicating why the function was unable to roll the dice / evaluate the expression.
 pub fn roll_dice<'a>(s: &'a str) -> Result<Roll, &'a str> {
     let s: String = s.split_whitespace().collect();
     let terms: Vec<DieRollTerm> = parse_die_roll_terms(&s);
@@ -180,7 +236,9 @@ fn parse_die_roll_terms(drex: &str) -> Vec<DieRollTerm> {
     terms
 }
 
-/// `roll_range()` will generate a random number within the specified range and return that value.
+/// Generates a random number within the specified range. Returns a `Result` containing
+/// either a valid signed 32-bit integer with the randomly generated number or some text 
+/// indicating the reason for failure.
 pub fn roll_range<'a>(min: i32, max: i32) -> Result<i32, &'a str> {
     if min > max {
         Err("Invalid range: min must be less than or equal to max")
